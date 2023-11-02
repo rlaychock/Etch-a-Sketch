797ce8481af1f60f000f3511514ce8421f03e668
Richard Laychock <rickylaychock155@gmail.com>
2023-11-02 17:35:50 -0400

    Added Changing Size of Grid Feature

--- javascript.js
+++ javascript.js
@@ -1,8 +1,19 @@
 let grid = document.querySelector('.grid');
+let color ="black";
 
-createGrid(16);
+const popup = document.getElementById('popup');
+popup.addEventListener('click', function(){
+    let newSize = getSize();
+    createGrid(newSize);
+});
+
+
 
 function createGrid(size) {
+
+    let squares = document.querySelectorAll('.gridItem');
+    squares.forEach((gridItem) => gridItem.remove()) ;
+
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
@@ -11,7 +22,28 @@ function createGrid(size) {
     for(i = 0; i < numSquares; i++) {
         const div = document.createElement('div');
         div.classList.add('gridItem');
+        
+        div.addEventListener('mouseover', () => {
+            div.style.backgroundColor = `${color}`;
+        });
         grid.insertAdjacentElement('beforeend', div);
     }
 }
 
+
+function getSize() {
+   input = prompt("Enter a board size (minimum 100): ");
+   message = document.querySelector(".message");
+   if(input === "") {
+    message.textContent = "Enter a number!";
+   } else if(input < 1 || input > 100){
+    message.textContent = "Enter a number between 1-100!";
+   } else {
+    message.textContent = "Good!";
+    return input;
+   }
+}   
+
+
+
+createGrid(16);
