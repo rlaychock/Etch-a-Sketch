99f8fdd1a2fe356d5e840c0333308fd0b4d35ece
Richard Laychock <rickylaychock155@gmail.com>
2023-11-01 20:19:07 -0400

    Added grid by manupulating DOM

--- javascript.js
+++ javascript.js
@@ -1,9 +1,17 @@
-const gridContainer = document.querySelector('#screen');
+let grid = document.querySelector('.grid');
 
-for(let i = 0; i < 16; i++) {
-    for(let j = 0; j < 16; j++) {
-        const gridItem = document.createElement('div');
-        gridItem.classList.add("grid-item");
-        gridContainer.appendChild(gridItem);
+createGrid(16);
+
+function createGrid(size) {
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    let numSquares = size * size;
+
+    for(i = 0; i < numSquares; i++) {
+        const div = document.createElement('div');
+        div.classList.add('gridItem');
+        grid.insertAdjacentElement('beforeend', div);
     }
 }
+
